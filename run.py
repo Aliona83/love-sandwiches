6a04a56e5b57c92f9be318b2833188b128577a1a
Aliona83 <alionajanusko@gmail.com>
2023-01-10 14:13:20 +0000

    Add user request loop to tepeat request for data until data provided is valid

--- run.py
+++ run.py
@@ -17,32 +17,40 @@ def get_sales_data():
     """
     Get sales figures input from the user
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here: ")
     
-    sales_data = data_str.split(",")
-    print(sales_data)
+        sales_data = data_str.split(",")
+   
+
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+
+    return sales_data
 
 
 def validate_data(values): 
     """
-
     Insite the try, converts all string values into integers.
     Raises ValueError if strings cannot be coverted into int,
     or if there aren't eaxctly 6 values.
     """
     print(values)
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
              f"Exacly 6 value required, you provided {len(values)}"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")       
+        return False
 
-
-get_sales_data()   
+    return True
+data = get_sales_data()   
     
